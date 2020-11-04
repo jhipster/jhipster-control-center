d8c72af9afe3cfc538c29d79843d6351fc571043
nassimerrahoui <nassim.et@hotmail.fr>
2020-11-04 12:48:42 +0100

    Add script to run app in ci

---
+++ .github/scripts/run-app-ci.sh
@@ -0,0 +1,29 @@
+#-------------------------------------------------------------------------------
+# Start docker container
+#-------------------------------------------------------------------------------
+
+if [ "$JHI_APP" == *"eureka"* && -a src/main/docker/jhipster-registry.yml ]; then
+    docker-compose -f src/main/docker/jhipster-registry.yml up -d
+    sleep 10
+    docker ps -a
+fi
+
+if [ "$JHI_APP" == *"consul"* && -a src/main/docker/consul.yml ]; then
+    docker-compose -f src/main/docker/consul.yml up -d
+    sleep 10
+    docker ps -a
+fi
+
+if [ "$JHI_APP" == *"oauth2"* && -a src/main/docker/keycloak.yml ]; then
+    docker-compose -f src/main/docker/keycloak.yml up -d
+    sleep 10
+    docker ps -a
+fi
+
+#-------------------------------------------------------------------------------
+# Run the application
+#-------------------------------------------------------------------------------
+
+java \
+    ./target/jhipster-control-center-*.jar \
+    --spring.profiles.active="$JHI_PROFILE" \
