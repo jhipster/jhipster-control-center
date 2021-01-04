a297645d59f74ddf2c5cf82296d08325b9384e95
nassimerrahoui <nassim.et@hotmail.fr>
2021-01-04 01:43:43 +0100

    improve render error message of vue abstract component

--- src/main/webapp/app/applications/abstract.component.ts
+++ src/main/webapp/app/applications/abstract.component.ts
@@ -6,16 +6,23 @@ export default class AbstractComponent extends Vue {
 
   renderErrorMessage(): string {
     return (
-      '<div class="alert alert-info col-md-8">' +
-      `<p><strong>Unable to reach this point for instance at the path: ${this.internalError.path}</strong></p>` +
+      '<div class="alert alert-info col-md-10">' +
+      `<p><strong>Unable to reach this endpoint for instance at the path: ${this.internalError.path}</strong></p>` +
       '<p>Please, be sure:</p>' +
       '<ul>' +
       '<li>the application is available</li>' +
+      '<br>' +
+      '<li>the application has the same base64-secret as the JHipster Control Center base64-secret' +
+      '<br><i>see property <code>jhipster.security.authentication.jwt.base64-secret</code> in application-*.yml files from src/main/resources/config</i></li>' +
+      '<br>' +
+      '<li>if you use Consul or Eureka from docker image, check if they have the same base64-secret as the JHipster Control Center base64-secret' +
+      '<br><i>see property <code>jhipster.security.authentication.jwt.base64-secret</code> in application-*.yml files from src/main/docker/central-server-config</i></li>' +
+      '<br>' +
       '<li>if the path is a spring boot actuator endpoint, verify if the endpoint is available in application.yml' +
       '<br><i>see property <code>management.endpoints.web.exposure.include</code></i></li>' +
       '</ul>' +
       '</div>' +
-      `<div class="alert alert-danger col-md-8">
+      `<div class="alert alert-danger col-md-10">
          <i>Error message: ${this.internalError.message}</i>
        </div>`
     );
@@ -24,7 +31,7 @@ export default class AbstractComponent extends Vue {
   set error(error: any) {
     const data = error.response?.data;
     this.internalError = {
-      message: data?.message,
+      message: data?.message + ' - ' + data?.detail,
       path: data?.path,
     } as RequestError;
   }
