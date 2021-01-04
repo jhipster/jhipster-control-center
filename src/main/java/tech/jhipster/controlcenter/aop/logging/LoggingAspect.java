dabb31130cb7892613cb8af702c21d44251957f7
Daniel Franco <dandrfranco@gmail.com>
2021-01-04 14:24:06 +0000

    Fix package

--- src/main/java/tech/jhipster/controlcenter/aop/logging/LoggingAspect.java
+++ src/main/java/tech/jhipster/controlcenter/aop/logging/LoggingAspect.java
@@ -16,6 +16,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package tech.jhipster.controlcenter.aop.logging;
+
 import java.util.Arrays;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
