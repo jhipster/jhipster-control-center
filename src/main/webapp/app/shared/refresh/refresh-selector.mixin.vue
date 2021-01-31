ebf0b53e2a0032576f58e9813f54a28c2f415b35
nassimerrahoui <nassim.et@hotmail.fr>
2021-01-31 20:27:00 +0100

    replace this <i> tag by <em>

--- src/main/webapp/app/shared/refresh/refresh-selector.mixin.vue
+++ src/main/webapp/app/shared/refresh/refresh-selector.mixin.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="dropdown">
     <button class="btn btn-outline-primary" v-on:click="manualRefresh()">
-      <span class="times-text">Refresh now <i class="fa fa-refresh"></i></span>
+      <span class="times-text">Refresh now <em class="fa fa-refresh"></em></span>
     </button>
     <b-dropdown ref="dd" variant="outline-primary" right>
-      <span slot="button-content"><i v-bind:class="classTime()"></i>{{ htmlActiveRefreshTime }} &nbsp;</span>
+      <span slot="button-content"><em v-bind:class="classTime()"></em>{{ htmlActiveRefreshTime }} &nbsp;</span>
       <span class="dropdown-header">Refresh times (in seconds)</span>
       <div v-for="time in refreshTimes" :key="time">
         <button
